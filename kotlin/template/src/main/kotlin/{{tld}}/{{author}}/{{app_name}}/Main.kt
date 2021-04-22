e61e85215949cea58f26b4daedca96e783f827de
Michel Silva <michel.silva.trd@c6bank.com>
2021-04-21 22:05:48 -0300

    start template repository with kotlin template

---
+++ kotlin/template/src/main/kotlin/{{tld}}/{{author}}/{{app_name}}/Main.kt
@@ -0,0 +1,8 @@
+package {{tld}}.{{author}}.{{app_name}}
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello {{author}} your program {{app_name}} is ready")
+    }
+}
---
+++ kotlin/template/src/test/kotlin/{{tld}}/{{author}}/{{app_name}}/MainKtTest.kt
@@ -0,0 +1,11 @@
+package {{tld}}.{{author}}.{{app_name}}
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+    @Test
+    fun `Teste`() {
+        assertTrue(true)
+    }
+}
